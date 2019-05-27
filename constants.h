b94c0456f6e3b67887c753d25afc143324f3acb3
Kevin Godell <kevin.godell@gmail.com>
2019-05-27 18:13:48 -0500

    automatic formatting

--- constants.h
+++ constants.h
@@ -1,41 +1,48 @@
 #include <string>
+
 using namespace std;
 
-enum CPUStates{
-	S_USER = 1,
-	S_NICE,
-	S_SYSTEM,
-	S_IDLE,
-	S_IOWAIT,
-	S_IRQ,
-	S_SOFTIRQ,
-	S_STEAL,
-	S_GUEST,
-	S_GUEST_NICE
+enum CPUStates {
+    S_USER = 1,
+    S_NICE,
+    S_SYSTEM,
+    S_IDLE,
+    S_IOWAIT,
+    S_IRQ,
+    S_SOFTIRQ,
+    S_STEAL,
+    S_GUEST,
+    S_GUEST_NICE
 };
 
-class Path{
+class Path {
 
 public:
     static string basePath() {
         return "/proc/";
     }
-    static string cmdPath(){
+
+    static string cmdPath() {
         return "/cmdline";
     }
-    static string statusPath(){
+
+    static string statusPath() {
         return "/status";
     }
-    static string statPath(){
+
+    static string statPath() {
         return "stat";
     }
-    static string upTimePath(){
+
+    static string upTimePath() {
         return "uptime";
     }
-    static string memInfoPath(){
+
+    static string memInfoPath() {
         return "meminfo";
     }
-    static string versionPath(){
+
+    static string versionPath() {
         return "version";
     }
 };
