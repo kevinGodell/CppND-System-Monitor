dadd0bf1b6b8aeafef6148e970910978fb683a98
Kevin Godell <kevin.godell@gmail.com>
2019-05-30 19:41:10 -0500

    remove using namespace std

--- constants.h
+++ constants.h
@@ -1,7 +1,5 @@
 #include <string>
 
-using namespace std;
-
 enum CPUStates {
     S_USER = 1,
     S_NICE,
@@ -18,31 +16,31 @@ enum CPUStates {
 class Path {
 
 public:
-    static string basePath() {
+    static std::string basePath() {
         return "/proc/";
     }
 
-    static string cmdPath() {
+    static std::string cmdPath() {
         return "/cmdline";
     }
 
-    static string statusPath() {
+    static std::string statusPath() {
         return "/status";
     }
 
-    static string statPath() {
+    static std::string statPath() {
         return "stat";
     }
 
-    static string upTimePath() {
+    static std::string upTimePath() {
         return "uptime";
     }
 
-    static string memInfoPath() {
+    static std::string memInfoPath() {
         return "meminfo";
     }
 
-    static string versionPath() {
+    static std::string versionPath() {
         return "version";
     }
 };
