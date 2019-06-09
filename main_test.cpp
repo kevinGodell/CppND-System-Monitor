59894163e009eadd0691622a3bd1bc3c6adb5cce
Kevin Godell <kevin.godell@gmail.com>
2019-06-09 18:14:26 -0500

    test methods

--- main_test.cpp
+++ main_test.cpp
@@ -13,7 +13,19 @@ int main() {
 
     std::string result = ProcessParser::getVmSize(pid);
 
-    std::cout << result << std::endl;
+    std::cout << "getVmSize " << result << std::endl;
+
+    result = ProcessParser::getProcUpTime(pid);
+
+    std::cout << "getProcUpTime " << result << std::endl;
+
+    result = ProcessParser::getCpuPercent(pid);
+
+    std::cout << "getCpuPercent " << result << std::endl;
+
+    result = ProcessParser::getProcUpTime(pid);
+
+    std::cout << "getSysUpTime " << ProcessParser::getSysUpTime(); << std::endl;
 
     return 0;
 }
