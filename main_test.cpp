232b5b68f4768952239d5448276175347e082cd5
Kevin Godell <kevin.godell@gmail.com>
2019-06-09 21:10:59 -0500

    format output

--- main_test.cpp
+++ main_test.cpp
@@ -5,57 +5,58 @@
 
 int main() {
 
-    std::string pid = "1";
+    //std::string pid = "1";
 
-    std::cout << "Enter pid: ";
+    //std::cout << "Enter pid: ";
 
-    std::cin >> pid;
-
-    //-----------------
-
-    std::string result = ProcessParser::getVmSize(pid);
-
-    std::cout << "getVmSize " << result << std::endl;
-
-    //-----------------
-
-    result = ProcessParser::getProcUpTime(pid);
-
-    std::cout << "getProcUpTime " << result << std::endl;
-
-    //-----------------
-
-    result = ProcessParser::getCpuPercent(pid);
-
-    std::cout << "getCpuPercent " << result << std::endl;
-
-    //-----------------
+    //std::cin >> pid;
 
     std::cout << "getSysUpTime " << ProcessParser::getSysUpTime() << std::endl;
-
-    //-----------------
-
-    result = ProcessParser::getProcUser(pid);
-
-    std::cout << "getProcUser " << result << std::endl;
-
-    //-----------------
-
-    std::vector<std::string> list = ProcessParser::getPidList();
-
-    std::cout << "getPidList size " << list.size() << std::endl;
-
-    //-----------------
-
-    result = ProcessParser::getCmd(pid);
-
-    std::cout << "getCmd " << result << std::endl;
 
     //-----------------
 
     std::cout << "getNumberOfCores " << ProcessParser::getNumberOfCores() << std::endl;
 
     //-----------------
+
+    std::vector<std::string> pidList = ProcessParser::getPidList();
+
+    std::cout << "getPidList size " << list.size() << std::endl;
+
+    for (const std:string & pid : pidList) {
+
+        //-----------------
+
+        std::string result = ProcessParser::getVmSize(pid);
+
+        std::cout << "getVmSize " << result << std::endl;
+
+        //-----------------
+
+        result = ProcessParser::getProcUpTime(pid);
+
+        std::cout << "getProcUpTime " << result << std::endl;
+
+        //-----------------
+
+        result = ProcessParser::getCpuPercent(pid);
+
+        std::cout << "getCpuPercent " << result << std::endl;
+
+        //-----------------
+
+        result = ProcessParser::getProcUser(pid);
+
+        std::cout << "getProcUser " << result << std::endl;
+
+        //-----------------
+
+        result = ProcessParser::getCmd(pid);
+
+        std::cout << "getCmd " << result << std::endl;
+
+        //-----------------
+    }
 
     return 0;
 }
