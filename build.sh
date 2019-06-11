73e0ab59e47c0a799c8423fa145799fa4147602e
Kevin Godell <kevin.godell@gmail.com>
2019-06-10 20:14:21 -0500

    push

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "pulling updates"
 git pull
-echo "building c++ files..."
+echo "building c++ files...."
 g++ -std=c++17 main_test.cpp ProcessParser.cpp Util.cpp SysInfo.cpp -o app
 echo "running program"
 ./app
