5dc971e9dca722d5e7215b989e298a81f3936b76
Kevin Godell <kevin.godell@gmail.com>
2019-06-13 05:58:47 -0500

    change build process

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "pulling updates"
+echo "pulling updates..."
 git pull
+echo "installing dependencies..."
+sudo apt-get install libncurses5-dev libncursesw5-dev --yes
 echo "compiling app..."
 g++ -std=c++17 main.cpp Process.cpp ProcessContainer.cpp ProcessParser.cpp SysInfo.cpp Util.cpp -o app -lncurses
-echo "running program"
+echo "running program..."
 ./app
