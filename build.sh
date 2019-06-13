343bcaf2fbc828a0780e90da30b7b3525ae10ad7
Kevin Godell <kevin.godell@gmail.com>
2019-06-12 19:27:30 -0500

    change build process

--- build.sh
+++ build.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 echo "pulling updates"
 git pull
-echo "compiling c++ files..."
-g++ -std=c++17 -c main.cpp
-g++ -std=c++17 -c Process.cpp
-g++ -std=c++17 -c ProcessContainer.cpp
-g++ -std=c++17 -c ProcessParser.cpp
-g++ -std=c++17 -c SysInfo.cpp
-g++ -std=c++17 -c Util.cpp
-echo "linking compiled files..."
-g++ -std=c++17 main.o Process.o ProcessContainer.o ProcessParser.o SysInfo.o Util.o -o app
+echo "compiling app..."
+g++ -std=c++17 main.cpp Process.cpp ProcessContainer.cpp ProcessParser.cpp SysInfo.cpp Util.cpp -o app -lncurses
 echo "running program"
 ./app
